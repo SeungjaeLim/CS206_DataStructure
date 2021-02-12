fd76fdf2e4f581a010d3fa518d21d34edadd05b7
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-02-12 14:18:59 +0900

    Data_type_Number

---
+++ java_language_vocabulary/Number.java
@@ -0,0 +1,24 @@
+package jump2java;
+
+public class Number 
+{
+	public static void main(String[] args) 
+	{
+		int max_int = 2147483647;
+		long max_long = 9223372036854775807L;
+		System.out.println(max_int);
+		System.out.println(max_long);
+		float pi = 3.14F;
+		double morepi = 3.14159265358979323846;
+		System.out.println(pi);
+		System.out.println(morepi);
+		double d1 = 123.4;
+		double d2 = 1.234e2;
+		System.out.println(d1);
+		System.out.println(d2);
+		int octal = 023;
+		int hex = 0xC;
+		System.out.println(octal);
+		System.out.println(hex);
+	}
+}
