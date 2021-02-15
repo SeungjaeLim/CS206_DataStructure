ae5d25068f18493e40a5633f8ff55d3a5833ad1e
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-02-15 22:34:55 +0900

    boolean in java

---
+++ java_language_vocabulary/Data_type_boolean.java
@@ -0,0 +1,26 @@
+package jump2java;
+
+public class Data_type_boolean 
+{
+	public static void main(String[] args) 
+	{
+		boolean isSuccess = true;
+		boolean isTest = false;
+		System.out.println(isSuccess);
+		System.out.println(isTest);
+		
+		System.out.println(2 > 1);
+		System.out.println(1 == 2 );
+		System.out.println(3 % 2 == 1);
+		System.out.println("3".equals("2"));
+		
+		int base = 180;
+		int height = 185;
+		boolean isTall = height > base;
+		if (isTall) 
+		{
+		    System.out.println("Å°°¡ Å®´Ï´Ù.");
+		}
+	}
+
+}
