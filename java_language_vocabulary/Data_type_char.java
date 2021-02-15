eb549ccde71971667837a0fd98fd2ac3da56a307
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-02-15 22:35:11 +0900

    char in java

---
+++ java_language_vocabulary/Data_type_char.java
@@ -0,0 +1,15 @@
+package jump2java;
+
+public class Data_type_char 
+{
+	public static void main(String[] args) 
+	{
+		char a1 = 'a'; //char value
+		char a2 = 97; //ASCII value
+		char a3 = '\u0061'; //UNICODE value
+
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+	}
+}
