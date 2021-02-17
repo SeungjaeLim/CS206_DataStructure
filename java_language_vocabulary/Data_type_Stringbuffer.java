83f42c30ae23d316285795f0c6d94f62f97419b7
SeungjaeLim <seungjaelim@kaist.ac.kr>
2021-02-17 23:22:25 +0900

    Stringbuffer in java

---
+++ java_language_vocabulary/Data_type_Stringbuffer.java
@@ -0,0 +1,14 @@
+package jump2java;
+
+public class Data_type_Stringbuffer 
+{
+	public static void main(String[] args) 
+	{
+		StringBuffer sb = new StringBuffer();
+        sb.append("hello");
+        sb.append(" ");
+        sb.append("jump to java");
+        System.out.println(sb.toString());
+	}
+
+}
---
+++ java_language_vocabulary/Data_type_Stringbuffer_replace_String.java
@@ -0,0 +1,14 @@
+package jump2java;
+
+public class Data_type_Stringbuffer_replace_String 
+{
+	public static void main(String[] args) 
+	{
+		String temp = "";
+        temp += "hello";
+        temp += " ";
+        temp += "jump to java";
+        System.out.println(temp);
+	}
+
+}
